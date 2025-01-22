a94a5bc5467a3b75733a06cdd096889497b6407c
Miguel Almudi <72586263+miguelAlmudi@users.noreply.github.com>
2025-01-22 00:08:40 -0300

    Add files via upload

--- xorCrypt.c
+++ xorCrypt.c
@@ -22,7 +22,45 @@ void xorCryptAlternative2(char* entrada, char* saida, char* senha) {
     saida[tamIn] = '\0';
 }
 
+void binToHex(char *bin, char *hex) {
+	int len = strlen(bin);
+	int value = 0;
+	for (int i = 0; i < len; i++) {
+		value = (value << 1) + (bin[i] - '0');
+			if ((i + 1) % 4 == 0) {
+				snprintf(hex + (i / 4), 2, "%X", value);
+				value = 0; 
+			}
+	} hex[len / 4] = '\0';
+}
 
+void hexToBin(char *hex, char *bin) {
+	char *lookup[] = {"0000", "0001", "0010", "0011",
+					  "0100", "0101", "0110", "0111",
+					  "1000", "1001", "1010", "1011",
+					  "1100", "1101", "1110", "1111"};
+  
+    int len = strlen(hex);
+	bin[0] = '\0'; // Inicializa a string binária como vazia
+	
+	for (int i = 0; i < len; i++) {
+		
+		if (hex[i] >= '0' && hex[i] <= '9') {
+			strcat(bin, lookup[hex[i] - '0']);
+		} 
+		else if (hex[i] >= 'A' && hex[i] <= 'F') {
+			strcat(bin, lookup[hex[i] - 'A' + 10]);
+		} 
+		else if (hex[i] >= 'a' && hex[i] <= 'f') {
+			strcat(bin, lookup[hex[i] - 'a' + 10]);
+		} 
+		else { 
+		// Caractere inválido 
+		printf("Caractere hexadecimal inválido: %c\n", hex[i]);
+		return;
+		}
+	} 
+}
 
 void stringToBinary(char *input, char *output) {
     
@@ -70,13 +108,14 @@ int main(){
     char decryptedBin[TAM2];
     char decrypted[TAM1];
     int selec = 1; 
+	char stringHex[TAM2];
+	char stringBin[TAM2];
+	char stringNote[TAM2];
 	
-	system("title XORCRYPT - BY: DIE");
-	system("color 2");
-    	
-	 while(selec == 1 || selec == 2){
+	    	
+	 while(selec == 1 || selec == 2 || selec == 3 || selec == 4 || selec == 5 || selec == 6){
 	 
-		 printf("Selecione um modo: 1 - Criptografar; 2 - Descriptografar \n");
+		 printf("Selecione um modo: 1 - Criptografar; 2 - Descriptografar; 3 - BinToHex Converter; 4 - HexToBin Converter; 5 - Anotar String \n");
 		 
 		 scanf("%d", &selec);
 		 printf("\n");
@@ -125,6 +164,47 @@ int main(){
 			printf("---------------------------------------------------\n");
 			
 		 }
+		 
+		 if(selec == 3){
+			 printf("---------------------------------------------------\n");
+			 printf("Binario Para Hexa: \n");
+			 printf("Insira o codigo Binario: \n");
+			 scanf("%8190s", stringBin);
+			 printf("---------------------------------------------------\n");
+			 binToHex(stringBin, stringHex);
+			 printf("%s\n", stringHex);
+			 printf("---------------------------------------------------\n");
+		 }
+		 
+		 if(selec == 4){
+			 printf("---------------------------------------------------\n");
+			 printf("Hexa Para Binario: \n");
+			 printf("Insira o codigo Hexa: \n");
+			 scanf("%8190s", stringHex);
+			 printf("---------------------------------------------------\n");
+			 hexToBin(stringHex, stringBin);
+			 printf("%s\n", stringBin);
+			 printf("---------------------------------------------------\n");
+		 }
+		 
+		 if(selec == 5){
+			 printf("---------------------------------------------------\n");
+			 printf("Insira o binario ou hexa para anotar: \n");
+			 scanf("%8190s", stringNote);
+			 printf("---------------------------------------------------\n");
+			 int tam = strlen(stringNote);
+			 int j = 0;
+			 for(int i = 0; i < tam; i++){
+				j++;
+				printf("%c", stringNote[i]);
+				if(j == 4){
+					printf("\n");
+					j = 0;
+				}
+			 }
+			 printf("\n");
+		 }
+		 if(selec == 6){system("cls");}
 	 
 	}
 
